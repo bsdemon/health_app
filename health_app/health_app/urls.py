74fc407417ac7f7bbbcbeb5804a84bfc58ef9d70
Ilia Iliev <ilia@magicsolutions.bg>
2017-07-18 00:19:36 +0300

    Added app url

--- health_app/health_app/urls.py
+++ health_app/health_app/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^image/', include('contrib.ocr.urls')),
+    url(r'^', include('contrib.ocr.urls')),
     url(r'^admin/', admin.site.urls),
 ]
