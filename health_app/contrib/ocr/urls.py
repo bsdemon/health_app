8337bae39a61c26d124c4e5c1d9f81e7051e2e3a
Ilia Iliev <ilia@magicsolutions.bg>
2017-07-18 00:18:43 +0300

    Added url to view

--- health_app/contrib/ocr/urls.py
+++ health_app/contrib/ocr/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^hello/$', views.index, name='index'),
+    url(r'^image/$', views.upload_file, name='upload_file')
 ]
