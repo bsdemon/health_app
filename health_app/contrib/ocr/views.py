ba193c040c229d18f8721f813e4a1b7a2ef6824f
Ilia Iliev <ilia@magicsolutions.bg>
2017-07-17 23:31:17 +0300

    Create hello world view

--- health_app/contrib/ocr/views.py
+++ health_app/contrib/ocr/views.py
@@ -1,22 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
-try:
-    import Image
-except ImportError:
-    from PIL import Image
+from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def image(request):
+    pass
+
+
+
+
+# pytesseract.pytesseract.tesseract_cmd = '<full_path_to_your_tesseract_executable>'
 # Include the above line, if you don't have tesseract executable in your PATH
 # Example tesseract_cmd: 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
 
-print(pytesseract.image_to_string(Image.open('test.png')))
-print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
-Add the following config, if you have tessdata error like: "Error opening data file..."
+# print(pytesseract.image_to_string(Image.open('test.png')))
+# print(pytesseract.image_to_string(Image.open('test-european.jpg'), lang='fra'))
 
-tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
-# Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
-# It's important to add double quotes around the dir path.
 
-pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
+# #Add the following config, if you have tessdata error like: "Error opening data file..."
+# tessdata_dir_config = '--tessdata-dir "<replace_with_your_tessdata_dir_path>"'
+# # Example config: '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+# # It's important to add double quotes around the dir path.
+
+# pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
